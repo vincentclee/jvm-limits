320c5f20546c3316cceb4993a73c555ba9a94524
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-08-11 12:47:36 -0400

    initial commit

---
+++ src/method_body/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The amount of code per non-native, non-abstract method is limited to 65536
+ * bytes by the sizes of the indices in the exception_table of the Code
+ * attribute (§4.7.3), in the LineNumberTable attribute (§4.7.8), and in the
+ * LocalVariableTable attribute (§4.7.9).
+ */
+/**
+ * @author vincentlee
+ * @since August 12, 2014
+ * @version 1.0
+ */
+package method_body;
